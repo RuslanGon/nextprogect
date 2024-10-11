1781d80e08785d7ccf7776ee50aeb641ef946648
RuslanGon <ruslan441006@gmail.com>
2024-10-11 16:28:07 +0300

    wer

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,18 @@
+import Image from 'next/image.js'
 import css from './page.module.css'
 
 const About = () => {
   return (
-    <div className={css.container}>About</div>
+    <div className={css.container}>
+      <div className={css.imgContainer}>
+      <Image
+          src="https://img.freepik.com/free-photo/paper-people-chain-on-green-grass-unity-concept_632805-35.jpg?w=1380&t=st=1685890833~exp=1685891433~hmac=46f15c2161ca525e238313ec9dc0812864411c64035b664d7abecd75e33494e1"
+          fill={true}
+          alt="about us"
+          className={css.img}
+        />
+      </div>
+    </div>
   )
 }
 
