8265728411f25f03c102de29da04e0d4d496a62f
RuslanGon <ruslan441006@gmail.com>
2024-10-11 17:04:33 +0300

    wer

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,9 +1,23 @@
-import css from "./page.module.css"
+import Link from 'next/link'
+import css from './page.module.css'
 
-const Porfolio = () => {
+const Portfolio = () => {
   return (
-    <div className={css.container}>Porfolio</div>
+    <div className={css.container}>
+    <h1 className={css.selectTitle}>Choose a gallery</h1>
+    <div className={css.items}>
+      <Link href="/portfolio/illustrations" className={css.item}>
+        <span className={css.title}>Illustrations</span>
+      </Link>
+      <Link href="/portfolio/websites" className={css.item}>
+        <span className={css.title}>Websites</span>
+      </Link>
+      <Link href="/portfolio/application" className={css.item}>
+        <span className={css.title}>Application</span>
+      </Link>
+    </div>
+  </div>
   )
 }
 
-export default Porfolio
+export default Portfolio
