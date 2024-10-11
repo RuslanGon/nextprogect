05b544f5dff06b7c96249177e7eb6c36fc4d57b5
RuslanGon <ruslan441006@gmail.com>
2024-10-11 17:41:20 +0300

    wer

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,8 +1,26 @@
+import Link from 'next/link.js'
 import css from './page.module.css'
+import Image from 'next/image.js'
 
 const Blog = () => {
   return (
-    <div className={css.container}>Blog</div>
+    <div className={css.mainContainer}>
+        <Link href='blog/test'className={css.container} key='1'>
+        <div className={css.imageContainer}>
+            <Image
+              // src={}
+              alt=""
+              width={400}
+              height={250}
+              className={css.image}
+            />
+          </div>
+          <div className={css.content}>
+            <h1 className={css.title}>Title</h1>
+            <p className={css.desc}>Desc</p>
+          </div>
+        </Link>
+    </div>
   )
 }
 
