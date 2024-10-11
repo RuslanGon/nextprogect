38930816cf7d71954d5edd52007574a76b83287c
RuslanGon <ruslan441006@gmail.com>
2024-10-11 16:52:09 +0300

    wer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,16 +8,16 @@ const Footer = () => {
         <div>2023 My App. All rights reserved</div>
         <div className={css.social}>
           <Link href="https://vk.com" target="_blank" passHref>
-            <Image className={css.icon} src='/vk.png' width={15} height={15} alt='VK icon' />
+            <Image className={css.icon} src='/vk.png' width={25} height={25} alt='VK icon' />
           </Link>
           <Link href="https://instagram.com" target="_blank" passHref>
-            <Image className={css.icon} src='/inst.png' width={15} height={15} alt='Instagram icon' />
+            <Image className={css.icon} src='/inst.png' width={25} height={25} alt='Instagram icon' />
           </Link>
           <Link href="https://twitter.com" target="_blank" passHref>
-            <Image className={css.icon} src='/twitter.png' width={15} height={15} alt='Twitter icon' />
+            <Image className={css.icon} src='/twitter.png' width={25} height={25} alt='Twitter icon' />
           </Link>
           <Link href="https://youtube.com" target="_blank" passHref>
-            <Image className={css.icon} src='/yt.png' width={15} height={15} alt='YouTube icon' />
+            <Image className={css.icon} src='/yt.png' width={25} height={25} alt='YouTube icon' />
           </Link>
         </div>
     </footer>
