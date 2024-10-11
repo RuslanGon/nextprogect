6f5ca350179d53ead754f494c65625315cdfb7b8
RuslanGon <ruslan441006@gmail.com>
2024-10-11 13:22:47 +0300

    wer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,16 +1,24 @@
+import Link from 'next/link'
 import css from './page.module.css'
-import Image from 'next/image.js'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
     <footer className={css.container}>
         <div>2023 My App. All rights reserved</div>
         <div className={css.social}>
-          <Image className={css.icon} src='/vk.png' width={15} height={15} alt='icon' />
-          <Image className={css.icon} src='/inst.png' width={15} height={15} alt='icon' />
-          <Image className={css.icon} src='/twitter.png' width={15} height={15} alt='icon' />
-          <Image className={css.icon} src='/yt.png' width={15} height={15} alt='icon' />
-
+          <Link href="https://vk.com" passHref>
+            <Image className={css.icon} src='/vk.png' width={15} height={15} alt='VK icon' />
+          </Link>
+          <Link href="https://instagram.com" passHref>
+            <Image className={css.icon} src='/inst.png' width={15} height={15} alt='Instagram icon' />
+          </Link>
+          <Link href="https://twitter.com" passHref>
+            <Image className={css.icon} src='/twitter.png' width={15} height={15} alt='Twitter icon' />
+          </Link>
+          <Link href="https://youtube.com" passHref>
+            <Image className={css.icon} src='/yt.png' width={15} height={15} alt='YouTube icon' />
+          </Link>
         </div>
     </footer>
   )
