3e913c55a52ede355a80e25aef0fc3d414eca822
RuslanGon <ruslan441006@gmail.com>
2024-10-11 17:25:11 +0300

    wer

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import Image from 'next/image'
 import css from './page.module.css'
+import Button from '@/components/Button/Button'
+import { items } from './data.js'
+import { notFound } from 'next/navigation';
 
-const Category = () => {
+const getData = (cat) =>{
+  const data = items[cat]
+
+  if (data){
+    data
+  }
+  return notFound()
+}
+
+
+const Category = ({params}) => {
+  // console.log(params);
+  const data = getData(params.category)
   return (
-    <div className={css.container}>Category</div>
+    <div className={css.container}>
+      <h1 className={css.catTitle}>{params.category}</h1>
+      {data.map((item)=>(
+      <div className={css.item} key={item.id}>
+        <div className={css.content}>
+          <h1 className={css.title}>{item.title}</h1>
+          <p className={css.desc}>{item.desc}</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={css.imgContainer}>
+          <Image
+            className={css.img}
+            fill={true}
+            src={item.image}
+             alt={item.title}
+          />
+        </div>
+      </div>
+
+      ))}
+
+
+    </div>
   )
 }
 
