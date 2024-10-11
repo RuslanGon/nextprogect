0dbab03f0e4da0f5f9945b88f4737992c8b371a2
RuslanGon <ruslan441006@gmail.com>
2024-10-11 12:47:15 +0300

    wer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,17 @@
 import css from './page.module.css'
+import Image from 'next/image.js'
 
 const Footer = () => {
   return (
     <footer className={css.container}>
         <div>2023 My App. All rights reserved</div>
-        <div></div>
+        <div className={css.social}>
+          <Image className={css.icon} src='/vk.png' width={15} height={15} alt='icon' />
+          <Image className={css.icon} src='/inst.png' width={15} height={15} alt='icon' />
+          <Image className={css.icon} src='/twitter.png' width={15} height={15} alt='icon' />
+          <Image className={css.icon} src='/yt.png' width={15} height={15} alt='icon' />
+
+        </div>
     </footer>
   )
 }
