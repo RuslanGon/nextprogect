0e07d61d2729743f840606966b05834496ea9805
RuslanGon <ruslan441006@gmail.com>
2024-10-13 12:49:13 +0300

    wer

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,12 @@
+"use client"
+import { useState } from 'react'
 import css from './page.module.css'
 
 const Dashboard = () => {
+  const [data, setData] = useState([])
+  const [error, setError] = useState(false)
+  const [isLoading, setIsLoading] = useState(alse)
+
   return (
     <div className={css.container}>Dashboard</div>
   )
