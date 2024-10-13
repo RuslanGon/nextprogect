d836d050fda498cad9b181fa674628c7418e3927
RuslanGon <ruslan441006@gmail.com>
2024-10-13 17:45:27 +0300

    wer

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -8,18 +8,16 @@ async function getData() {
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
+  
   const data = await res.json();
-
-  // Проверка, если данные - это строка, то парсим её
-  return typeof data === "string" ? JSON.parse(data) : data;
+  return data.posts || data; // Измените на правильный ключ в зависимости от структуры данных
 }
 
 const Blog = async () => {
   const data = await getData();
-  console.log(data); // Логируем ответ API
 
-  const posts = Array.isArray(data) ? data : []; // Измените на правильный ключ в зависимости от структуры данных
-  
+  // Проверка типа данных
+  const posts = Array.isArray(data) ? data : [];
   if (!Array.isArray(posts)) {
     throw new Error("Expected an array of posts");
   }
@@ -29,12 +27,13 @@ const Blog = async () => {
       {posts.map((item) => (
         <Link href={`blog/${item._id}`} className={css.container} key={item._id}>
           <div className={css.imageContainer}>
-            <Image src={item.img} alt="foto" width={400} height={250} className={css.image} />
+            {/* Если у вас есть изображения в папке public/img, используйте этот путь */}
+            <Image src={`/img/${item.img}`} alt="foto" width={400} height={250} className={css.image} />
           </div>
           <div className={css.content}>
             <h1 className={css.title}>{item.title}</h1>
             <p className={css.desc}>{item.body}</p>
-            <p className={css.desc}> Avtor: {item.username}</p>
+            <p className={css.desc}>Avtor: {item.username}</p>
           </div>
         </Link>
       ))}
