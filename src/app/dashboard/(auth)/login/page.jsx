30a708a81b828cb5d13c79a6e36c8b304d3619b4
RuslanGon <ruslan441006@gmail.com>
2024-10-14 16:54:01 +0300

    wer

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -1,8 +1,12 @@
+"use client"
+import { signIn } from 'next-auth/react'
 import css from './page.module.css'
 
 const Login = () => {
   return (
-    <div className={css.container}>Login</div>
+    <div className={css.container}>
+      <button onClick={() => signIn("google")}>login with Google</button>
+    </div>
   )
 }
 
--- src/app/dashboard/(auth)/registor/page.jsx
+++ src/app/dashboard/(auth)/registor/page.jsx
@@ -1,9 +1,26 @@
+"use client"
+import Link from 'next/link';
 import css from './page.module.css'
 
 const Registor = () => {
   return (
-    <div className={css.container}>Registor</div>
-  )
-}
+    <div className={css.container}>
+    <h1 className={css.title}>Create an Account</h1>
+    <h2 className={css.subtitle}>Please sign up to see the dashboard.</h2>
+    <form  className={css.form}>
+      <input type="text" placeholder="Username" required className={css.input}/>
+      <input type="text" placeholder="Email" required className={css.input}/>
+      <input type="password" placeholder="Password" required className={css.input}
+      />
+      <button className={css.button}>Register</button>
+      {/* {error && "Something went wrong!"} */}
+    </form>
+    <span className={css.or}>- OR -</span>
+    <Link className={css.link} href="/dashboard/login">
+      Login with an existing account
+    </Link>
+  </div>
+);
+};
 
-export default Registor
+export default Registor;
