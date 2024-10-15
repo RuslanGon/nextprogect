3bddfdfa7fac0c122b16d1fe456d940c33edd274
RuslanGon <ruslan441006@gmail.com>
2024-10-15 16:40:43 +0300

    wer

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import CredentialPovider from 'next-auth/providers/credentials'
+import CredentialsProvider from "next-auth/providers/credentials";
+import connect from "@/utils/db";
+import User from "@/models/User";
+import bcrypt from "bcrypt";
 
 const handler = NextAuth({
   providers: [
@@ -8,8 +11,35 @@ const handler = NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    CredentialPovider()
+
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      async authorize(credentials) {
+        await connect();
+
+        try {
+          const user = await User.findOne({ email: credentials.email });
+
+          if (user) {
+            const isPasswordCorrect = await bcrypt.compare(
+              credentials.password,
+              user.password
+            );
+            if (isPasswordCorrect) {
+              return user;
+            } else {
+              throw new Error("Wrong credentials");
+            }
+          } else {
+            throw new Error("User not found");
+          }
+        } catch (error) {
+          throw new Error(error.message);
+        }
+      },
+    }),
   ],
 });
 
-export {handler as GET, handler as POST}
+export { handler as GET, handler as POST };
