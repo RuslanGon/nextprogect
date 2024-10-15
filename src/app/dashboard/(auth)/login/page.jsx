52cdaf9795483aee3bd8517e769a1c6d322947de
RuslanGon <ruslan441006@gmail.com>
2024-10-15 16:50:04 +0300

    wer

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -1,13 +1,33 @@
 "use client"
-import { signIn } from 'next-auth/react'
+import { signIn, useSession } from 'next-auth/react'
 import css from './page.module.css'
+import { useRouter } from 'next/router.js'
 
 const Login = () => {
-  return (
-    <div className={css.container}>
-      <button onClick={() => signIn("google")}>login with Google</button>
-    </div>
-  )
-}
+const session = useSession()
+const router = useRouter()
 
-export default Login
+return (
+  <div className={css.container}>
+    <form onSubmit={handleSubmit} className={css.form}>
+      <input
+        type="email"
+        placeholder="Email"
+        required
+        className={css.input}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        required
+        className={css.input}
+      />
+      <button className={css.button}>Login</button>
+    </form>
+
+    <button onClick={() => signIn("google")}>Login with Google</button>
+  </div>
+);
+};
+
+export default Login;
