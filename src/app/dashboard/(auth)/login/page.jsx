87b57e28f3b2dd017411103ec742443acfbb032c
RuslanGon <ruslan441006@gmail.com>
2024-10-15 17:44:34 +0300

    wer

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -1,30 +1,40 @@
 "use client"
-import { signIn, useSession } from 'next-auth/react'
-import css from './page.module.css'
-import { useRouter } from 'next/router.js'
+import { signIn, useSession } from 'next-auth/react';
+import css from './page.module.css';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 const Login = () => {
-const session = useSession()
-// const router = useRouter()
-console.log(session);
+  const { status, data: session } = useSession();
+  const router = useRouter();
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  const email = e.target[0].value
-  const password = e.target[1].value
-  signIn('credentials', {email, password})
-}
+  useEffect(() => {
+    if (status === "authenticated") {
+      router.push("/");
+    }
+  }, [status, router]);
 
-return (
-  <div className={css.container}>
-    <form onSubmit={handleSubmit} className={css.form}>
-      <input type="email" placeholder="Email" required className={css.input}/>
-      <input type="password" placeholder="Password" required className={css.input}/>
-      <button className={css.button}>Login</button>
-    </form>
-    <button onClick={() => signIn("google")}>Login with Google</button>
-  </div>
-);
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+    signIn('credentials', { email, password });
+  };
+
+  return (
+    <div className={css.container}>
+      <form onSubmit={handleSubmit} className={css.form}>
+        <input type="email" placeholder="Email" required className={css.input} />
+        <input type="password" placeholder="Password" required className={css.input} />
+        <button className={css.button}>Login</button>
+      </form>
+      <button onClick={() => signIn("google")}>Login with Google</button>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,55 +1,60 @@
-"use client"
-import Link from "next/link"
-import css from './page.module.css'
+"use client";
+import Link from "next/link";
+import css from "./page.module.css";
 import DarckModelToggle from "../DarckModelToggle/DarckModelToggle.jsx";
+import { signOut, useSession } from "next-auth/react";
 
 const links = [
-    {
-      id: 1,
-      title: "Home",
-      url: "/",
-    },
-    {
-      id: 2,
-      title: "Portfolio",
-      url: "/portfolio",
-    },
-    {
-      id: 3,
-      title: "Blog",
-      url: "/blog",
-    },
-    {
-      id: 4,
-      title: "About",
-      url: "/about",
-    },
-    {
-      id: 5,
-      title: "Contact",
-      url: "/contact",
-    },
-    {
-      id: 6,
-      title: "Dashboard",
-      url: "/dashboard",
-    },
-  ];
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "Portfolio",
+    url: "/portfolio",
+  },
+  {
+    id: 3,
+    title: "Blog",
+    url: "/blog",
+  },
+  {
+    id: 4,
+    title: "About",
+    url: "/about",
+  },
+  {
+    id: 5,
+    title: "Contact",
+    url: "/contact",
+  },
+  {
+    id: 6,
+    title: "Dashboard",
+    url: "/dashboard",
+  },
+];
 
 const Navbar = () => {
+  const session = useSession();
+
   return (
     <nav className={css.container}>
-        <Link className={css.logo} href='/'>My App</Link>
-        <div className={css.links}>
-          <DarckModelToggle />
-            {links.map(link => (
-                <Link key={link.id} href={link.url}>{link.title}</Link>
-            ))}
-            <button className={css.logout} onClick={() => {console.log('logout')}}>Logout</button>
-        </div>
+      <Link className={css.logo} href="/"> My App </Link>
+      <div className={css.links}><DarckModelToggle />
+        {links.map((link) => (
+          <Link key={link.id} href={link.url}>
+            {link.title}
+          </Link>
+        ))}
+        {session == "authenticated" && (
+          <button className={css.logout} onClick={signOut}>Logout </button>
+        )}
+      </div>
     </nav>
-    
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
