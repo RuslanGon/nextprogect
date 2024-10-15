1c136107533f980a2ce189aa6f4bfd88509a14b7
RuslanGon <ruslan441006@gmail.com>
2024-10-15 17:04:17 +0300

    wer

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -5,26 +5,23 @@ import { useRouter } from 'next/router.js'
 
 const Login = () => {
 const session = useSession()
-const router = useRouter()
+// const router = useRouter()
+console.log(session);
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const email = e.target[0].value
+  const password = e.target[1].value
+  signIn('credentials', {email, password})
+}
 
 return (
   <div className={css.container}>
     <form onSubmit={handleSubmit} className={css.form}>
-      <input
-        type="email"
-        placeholder="Email"
-        required
-        className={css.input}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        required
-        className={css.input}
-      />
+      <input type="email" placeholder="Email" required className={css.input}/>
+      <input type="password" placeholder="Password" required className={css.input}/>
       <button className={css.button}>Login</button>
     </form>
-
     <button onClick={() => signIn("google")}>Login with Google</button>
   </div>
 );
