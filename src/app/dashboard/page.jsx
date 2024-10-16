8a10dde64c3b054edab42e124e679bd212bb4123
RuslanGon <ruslan441006@gmail.com>
2024-10-16 16:24:30 +0300

    wer

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,39 +1,126 @@
-"use client"
-import useSWR from 'swr'
-// import { useEffect, useState } from 'react'
-import css from './page.module.css'
-import { useSession } from 'next-auth/react'
+"use client";
+
+import useSWR from "swr";
+import { useState, useEffect } from "react";
+import css from "./page.module.css";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 const Dashboard = () => {
-  const session = useSession()
-  console.log(session);
-  // const [data, setData] = useState([])
-  // const [error, setError] = useState(false)
-  // const [isLoading, setIsLoading] = useState(true)
-  
+  // const [data, setData] = useState([]);
+  // const [error, setError] = useState(false);
+  // const [isLoading, setIsLoading] = useState(true);
+
   // useEffect(() => {
   //   const getData = async () => {
-  //     const res = await fetch("https://jsonplaceholder.typicode.com/posts", {cache: "no-store",});
+  //     const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+  //       cache: "no-store",
+  //     });
+
   //     if (!res.ok) {
-  //       setError(true)
-  //       throw new Error("Failed to fetch data");
+  //       setError(true);
   //     }
   //     const data = await res.json()
+
   //     setData(data)
   //     setIsLoading(false)
   //   };
   //   getData()
-  // }, []);
+  // },[]);
 
   // console.log(data);
 
-  const fetcher = (...args) => fetch(...args).then(res => res.json())
-  const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
-  // console.log(data);
+  const session = useSession();
+  const router = useRouter();
 
-  return (
-    <div className={css.container}>Dashboard</div>
-  )
-}
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, mutate, error, isLoading } = useSWR(
+    `/api/posts?username=${session?.data?.user.name}`,
+    fetcher
+  );
 
-export default Dashboard
+  if (session.status == "loading") {
+    return <p>Loading...</p>;
+  }
+  if (session.status == "unauthenticated") {
+    router?.push("/dashboard/login");
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const title = e.target[0].value;
+    const desc = e.target[1].value;
+    const img = e.target[2].value;
+    const content = e.target[3].value;
+
+    try {
+      await fetch("/api/posts", {
+        method: "POST",
+        body: JSON.stringify({
+          title,
+          desc,
+          img,
+          content,
+          username: session.data.user.name,
+        }),
+      });
+      mutate();
+      e.target.reset();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await fetch(`/api/posts/${id}`, {
+        method: "DELETE",
+      });
+      mutate();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  if (session.status == "authenticated") {
+    return (
+      <div className={css.container}>
+        <div className={css.posts}>
+          {isLoading
+            ? "Loading"
+            : data?.map((post) => (
+                <div className={css.post} key={post._id}>
+                  <div className={css.imgContainer}>
+                    <Image
+                      src={post.img}
+                      alt={post.title}
+                      width={200}
+                      height={100}
+                    />
+                  </div>
+                  <h2 className={css.postTitle}>{post.title}</h2>
+                  <span
+                    className={css.delete}
+                    onClick={() => handleDelete(post._id)}
+                  >
+                    X
+                  </span>
+                </div>
+              ))}
+        </div>
+
+        <form className={css.new} onSubmit={handleSubmit}>
+          <h1>Add New Post</h1>
+          <input type="text" placeholder="Title" className={css.input} />
+          <input type="text" placeholder="Desc" className={css.input} />
+          <input type="text" placeholder="Image" className={css.input} />
+          <textarea cols="30" rows="10" className={css.textArea} />
+          <button className={css.button}> Send</button>
+        </form>
+      </div>
+    );
+  }
+};
+
+export default Dashboard;
